21539e2cfed122c27a934bd097077e1e404995df
Sivabalan-G <93809956+Sivabalan-G@users.noreply.github.com>
2021-11-28 09:23:40 +0530

    Update wbsort.java

--- wbsort.java
+++ wbsort.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javasort;
 import java.util.*;
-/**
- *
- * @author USERi
- */
 public class wbsort {
     public static void main(String[] args){
         Scanner p=new Scanner(System.in);
-        int[] arr=new int[5];
-        for(int i=0;i<5;i++){
+        int[] arr=new int[5];           //new int[] {78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -65}; 
+        for(int i=0;i<5;i++){           //i<arr.length
             arr[i]=p.nextInt();
         }
         System.out.println("Before sorting:");
